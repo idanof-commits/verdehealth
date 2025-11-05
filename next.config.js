3da6881bf60c17fb229c7c39da6d401178fe3c43
idanof-commits <i.danof@gmail.com>
2025-11-05 22:52:32 +0200

    Simplify Next.js configuration
    Removed strict mode and redirects from Next.js configuration.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      { source: "/:path*", has: [{ type: "host", value: "www.verdehealth.bg" }], destination: "https://verdehealth.bg/:path*", permanent: true },
-      { source: "/:path*", has: [{ type: "host", value: "verdehealth.eu" }], destination: "https://verdehealth.bg/:path*", permanent: true }
-    ];
-  }
-};
+const nextConfig = {};
 module.exports = nextConfig;
