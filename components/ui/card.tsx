cb4625e50e5b323607700badb7aa6a42fe4e42a3
idanof-commits <i.danof@gmail.com>
2025-11-06 00:00:57 +0200

    Add Card and CardContent components

---
+++ components/ui/card.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export function Card({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`rounded-2xl border bg-white shadow-md ${className}`} {...props} />;
+}
+export function CardContent({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`p-6 ${className}`} {...props} />;
+}
